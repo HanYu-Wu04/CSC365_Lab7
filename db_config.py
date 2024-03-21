55d8a41f4a5a79d6fe8d5f2cb457c332f80c90d5
HanYu Wu <hanyuwu04@gmail.com>
2024-03-20 23:52:02 -0700

    setting up database requirement and config file

---
+++ db_config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+#load environment variables from .env file
+load_dotenv()
+
+db_config = {
+    'host': 'db.labthreesixfive.com',
+    'user': 'hwu35',
+    'password': 'your_password',
+    'database': 'your_database_name'
+}
